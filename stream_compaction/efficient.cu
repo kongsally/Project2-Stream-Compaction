98f638467ed44526c60070009036c697cfbf1508
Sally Kong <kongsally@users.noreply.github.com>
2015-09-13 20:18:54 -0400

    work efficient non power of two working

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -34,33 +34,34 @@ __global__ void downSweep(int n, int d, int *o_data, int *i_data) {
 
 void scan(int n, int *odata, const int *idata) {
     int d = ilog2ceil(n);
+	int total = (int) pow(2.0, d);
 
-	cudaMalloc((void**)&scan_result, n * sizeof(int));
-	cudaMalloc((void**)&temp_scan, n * sizeof(int));
+	cudaMalloc((void**)&scan_result, total * sizeof(int));
+	cudaMalloc((void**)&temp_scan, total * sizeof(int));
 	
-	cudaMemcpy(temp_scan, idata, n * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(scan_result, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_scan, idata, total * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(scan_result, idata, total * sizeof(int), cudaMemcpyHostToDevice);
 
-	dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+	dim3 fullBlocksPerGrid((total + blockSize - 1) / blockSize);
 	
 	for (int i = 0; i < d; i++) {
-		upSweep<<<fullBlocksPerGrid, blockSize>>>(n, i, scan_result, temp_scan);
+		upSweep<<<fullBlocksPerGrid, blockSize>>>(total, i, scan_result, temp_scan);
 		temp_scan = scan_result;
 	}
 
 	
-	cudaMemcpy(odata, scan_result, n * sizeof(int), cudaMemcpyDeviceToHost);
-	odata[n-1] = 0;
+	cudaMemcpy(odata, scan_result, total * sizeof(int), cudaMemcpyDeviceToHost);
+	odata[total-1] = 0;
 
-	cudaMemcpy(scan_result, odata, n * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(temp_scan, odata, n * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(scan_result, odata, total * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(temp_scan, odata, total * sizeof(int), cudaMemcpyHostToDevice);
 
 	for (int i = d-1; i >= 0; i--) {
-		downSweep<<<fullBlocksPerGrid, blockSize>>>(n, i, scan_result, temp_scan);
+		downSweep<<<fullBlocksPerGrid, blockSize>>>(total, i, scan_result, temp_scan);
 		temp_scan = scan_result;
 	}
 
-	cudaMemcpy(odata, scan_result, n * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(odata, scan_result, total * sizeof(int), cudaMemcpyDeviceToHost);
 	cudaFree(scan_result);
 	cudaFree(temp_scan);
 
